f0e41225c1eadf3572f09e2377ef1dd38c5f7921
EL FADIL MOHAMED <medelfadil193@gmail.com>
2024-05-01 14:10:56 +0100

    first commit

---
+++ src/main/java/org/java/demo/projet_fin_module/ProjetFinModuleApplication.java
@@ -0,0 +1,13 @@
+package org.java.demo.projet_fin_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFinModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetFinModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/java/demo/projet_fin_module/ProjetFinModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.demo.projet_fin_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFinModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
